d1754b67f5bc95ccdfd40c02e3deddc551a962b6
JAX <netspec1@hotmail.com>
2017-07-12 15:33:15 -0400

    switch started

---
+++ SwitchPractice1/SwitchPractice1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color");
+            string color = Console.ReadLine();
+
+
+            switch(color)
+            {
+
+                case "red":
+                    Console.WriteLine("Red is an ok Color");
+                    break;
+
+                default:
+                    Console.WriteLine("We don't understand your answer");
+                    break;
+                    
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
