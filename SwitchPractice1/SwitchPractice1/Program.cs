5c5c1e7b534c0c6962e1eeadd5c5e4dd76981eea
JAX <netspec1@hotmail.com>
2017-07-13 11:42:57 -0400

    added bool and tryparse to switch practice 1 project

--- SwitchPractice1/SwitchPractice1/Program.cs
+++ SwitchPractice1/SwitchPractice1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace SwitchPractice1
 {
@@ -37,9 +38,44 @@ namespace SwitchPractice1
             }
             Console.Write("We broke out of the loop");
             Console.ReadLine();
+
+
+
+
+            bool yes;
+            int foodLike;
+            Console.WriteLine("What's is your favorite pasta dish \n 1. Lasagne \n 2. Spaghetti \n 3. Ravioli");
+            //int foodChoice = Convert.ToInt32(ReadLine().ToUpper());
+            string foodChoice = Console.ReadLine();
+            //int foodChoice = int.Parse(ReadLine());
+           int.TryParse(foodChoice, out foodLike);
+           // double food = Convert.ToDouble(ReadLine().ToUpper());
+
+            switch(foodLike)
+            {
+                case 1:
+                    Console.WriteLine("Lasagne is fantastic!!!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Spaghetti is delicious");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Ravioli was invented by Chev Boyardi!!!");
+                    break;
+
+                default:
+                    Console.WriteLine("That's not a pasta choice we recognize");
+                    break;
+
+
+
+            }
+            Console.WriteLine("We broke out of the switch statement!!");
+            Console.ReadLine();
         }
-
-
+        
         public static void Parsing()
         {
             
