c2ad3cc98572e08cc8ba92999f14d61565c36ea7
csharpspecialist <netspec1@hotmail.com>
2017-07-17 19:23:13 -0400

    added an implicit and explicit conversions app for class usage

---
+++ ImplicitAndExplicitConversions1/ImplicitAndExplicitConversions1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ImplicitAndExplicitConversions1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter();
+
+
+
+        }
+
+        private static void Converter()
+        {
+            byte destinationVar;
+            short sourceVar = 7;
+            destinationVar = (byte)sourceVar;  //explicit conversion here!!!
+            WriteLine($"sourceVar val: {sourceVar}");
+            WriteLine($"destinationVar val: {destinationVar}");
+            ReadKey();
+
+
+            byte bVar;
+            short shortVar = 281;
+            bVar = (byte)shortVar;  //explicit conversion here!!!
+            WriteLine($"sourceVar val: {shortVar}");
+            WriteLine($"destinationVar val: {bVar}");
+            ReadKey();
+        }
+    }
+}
