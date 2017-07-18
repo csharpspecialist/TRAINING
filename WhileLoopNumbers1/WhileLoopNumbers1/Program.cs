80da07a7bf627d6926935d67f4bc89d18697f705
JAX <netspec1@hotmail.com>
2017-07-18 11:03:37 -0400

    created while loops numbers and while loop practice 1 project

---
+++ WhileLoopNumbers1/WhileLoopNumbers1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopNumbers1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idNum;
+            int lowNum = 1000;
+            int highNum = 2000;
+
+            Console.WriteLine("Enter a valid Id Number");
+            string valNum = Console.ReadLine();
+            int.TryParse(valNum, out idNum);
+
+
+            while(idNum < lowNum || idNum > highNum)
+            {
+                Console.WriteLine($"The ID number you entered {valNum} \n " 
+                    + $"must be between {lowNum} and {highNum} \n Try again");
+
+                valNum = Console.ReadLine();
+                int.TryParse(valNum, out idNum);
+            }
+
+            Console.WriteLine("The id number that you entered was valid!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WhileLoopPractice1/WhileLoopPractice1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "bob";
+            string password = "agent";
+
+            Console.WriteLine("What is your username");
+            string inputName = Console.ReadLine();
+
+
+            Console.WriteLine("what is your password");
+            string pw = Console.ReadLine();
+                    
+
+            while (inputName == userName && pw == password)
+            {
+                Console.WriteLine($"The combination that you entered was not correct");
+                Console.WriteLine("Try again");
+                inputName = Console.ReadLine();
+                pw = Console.ReadLine();
+            }
+
+            Console.WriteLine("That was the correct username... and password..Hooray");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
