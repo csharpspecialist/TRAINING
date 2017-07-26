1c9fc23244a9167a8d97c8859449b02bdc80c674
JAX <netspec1@hotmail.com>
2017-07-26 08:39:47 -0400

    finished the optional parameters and passing data types projects

---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Using 2 arguments here!!!");
+            DisplaySize(10, 20);
+            Console.WriteLine("Using 3 arguments now");
+            DisplaySize(1, 2, 3);
+            ReadLine();
+
+
+        }
+
+        public static void DisplaySize(int length, int width, int height =1)
+        {
+            int area = length * width * height;
+            Console.WriteLine($"the length {length} * the width {width} * the optional height of {height} equals {area}");
+            ReadLine();
+
+        }
+
+
+
+    }
+}
---
+++ PassingDataTypes/PassingDataTypes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayInt = { 1, 5, 12, 66, 27 };
+            string[] nameArray = { "Nancy Drew", "Columbo","Hardy Boys" };
+
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                Console.WriteLine("The value of the Arrayint array is " + arrayInt[i]);
+            }
+
+            Console.WriteLine("we r now out of th loop");
+            Console.ReadLine();
+
+            MaxArray(arrayInt);
+            SumArray(arrayInt);
+            ListNames(nameArray);
+        }
+
+
+        public static void SumArray(int [] arrayVals)
+        {
+            int sumation = arrayVals.Sum();
+            Console.WriteLine($"the sum of the array is {sumation}");
+            Console.ReadLine();     
+            
+        }
+
+        public static void MaxArray(int [] intArray)
+        {
+            int maxVal = intArray.Max();
+            Console.WriteLine($"The maximum value of the array is {maxVal} ");
+            Console.ReadLine();
+
+        }
+
+        public static void ListNames(string [] Names)
+        {
+            for(int k = 0; k < Names.Length; k++  )
+            {
+                Console.WriteLine("The names of the ArrayNames array are " + Names[k]);
+            }
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
