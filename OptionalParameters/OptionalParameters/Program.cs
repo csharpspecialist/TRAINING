5cb826b980c9e192f612806a57a3a8e24111d06a
JAX <netspec1@hotmail.com>
2017-07-26 09:15:41 -0400

    added additional optional parameters to the optional parameters project

--- OptionalParameters/OptionalParameters/Program.cs
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -17,17 +17,26 @@ namespace OptionalParameters
             Console.WriteLine("Using 3 arguments now");
             DisplaySize(1, 2, 3);
             ReadLine();
-
+            Practice(2, "Latte");
+            ReadLine();
 
         }
 
-        public static void DisplaySize(int length, int width, int height =1)
+        public static void DisplaySize(int length, int width, int height =1, string shirts = "")
         {
             int area = length * width * height;
             Console.WriteLine($"the length {length} * the width {width} * the optional height of {height} equals {area}");
             ReadLine();
 
         }
+
+
+        public static void Practice(double nice, string hamburger = "")
+        {
+
+            WriteLine($"we brought in the number {nice} but we like {hamburger}s to drink in the morning");
+        }
+
 
 
 
