fa96a4ede356912d38d16e1477c1ce6d5c699caa
csharpspecialist <netspec1@hotmail.com>
2017-07-27 02:55:31 -0400

    checked the debug exercises for 6-1 and 6-2

--- DebuggingExercises3/DebuggingExercises3/Program.cs
+++ DebuggingExercises3/DebuggingExercises3/Program.cs
@@ -13,7 +13,9 @@ namespace DebuggingExercises3
             //  DebugFive1.FiveOne();
             // DebugFive2.FiveTwo();
             //DebugFive3.FiveThree();
-            DebugFive4.FiveFour();
+            // DebugFive4.FiveFour();
+            // DebugSix01.SixOne();
+            DebugSix02.SixTwoMain();
 
         }
     }
