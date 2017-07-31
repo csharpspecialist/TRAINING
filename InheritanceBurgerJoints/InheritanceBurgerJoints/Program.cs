ffc8914ce0b14180014e6ee8a32c274af16128ec
csharpspecialist <netspec1@hotmail.com>
2017-07-31 03:06:06 -0400

    pretty much fin W Inheritance Projs for class

---
+++ InheritanceBurgerJoints/InheritanceBurgerJoints/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceBurgerJoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mcdonalds mc = new Mcdonalds();
+            mc.BurgerName = "Big Mac";
+            mc.SizeOfFries = " Small Fry";           
+            mc.DrinkSize = "Small";
+            mc.DrinkName = "Sprite";
+            
+            mc.DailyOrder();
+
+            Wendys wnd = new Wendys();
+
+            wnd.BurgerName = "Single";
+            wnd.SizeOfFries = "Small Fry";
+            wnd.DrinkSize = "Medium";
+            wnd.DrinkName = "Tea";
+
+            wnd.DailyOrder();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+    //Base Class
+    public class GeneralFoods
+    {
+        
+        public string BurgerName;
+        public string SizeOfFries;
+        public int NuggetsKids;
+        public int NuggetsSmall;
+        public int NuggetsMed;
+        public int NuggetsLarge;
+        public string DrinkName;
+        public string DrinkSize;
+     
+        
+        public virtual void DailyOrder() //don't mark this virtual at first
+        {
+            Console.WriteLine($"You ordered a \n {BurgerName} " +
+            $"plus a {SizeOfFries} and a {DrinkSize} {DrinkName}");
+
+
+        }
+
+    
+
+    }
+
+
+    public class Mcdonalds : GeneralFoods
+    {
+
+        public string BM;
+        public string Qp;
+        public string Dqp;       
+
+    }
+
+    public class Wendys : GeneralFoods
+    {
+
+        public string Single;
+        public string Double;
+        public string IceCream;
+
+        public override void DailyOrder() //override later in class!!!!
+        {
+            base.DailyOrder();
+            Console.WriteLine($"You really ordered something else...");
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ InheritancePrac1/InheritancePrac1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePrac1
+{
+    public class Employee
+    {
+        //Base Class
+       public string FirstName;
+        public string LastName;
+        public int EmployeeId;
+        public string Email;
+       
+        public void PrintFullName()
+        {
+           
+            Console.WriteLine($"my First name is {FirstName} and my last name is {LastName}");
+
+        }
+
+
+    }
+    public class FullTimeEmployee : Employee
+    {
+        public double Salary;
+        public double FullTimeID;
+        public void PrintName()
+        {
+            Employee e = new Employee();
+            e.PrintFullName();
+
+        }
+
+
+
+    }
+
+
+    public class PartTimeEmployee : Employee
+    {
+        Employee e = new Employee();
+        public double HourlyRate;
+        public int PartTimeId;
+
+    
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FullTimeEmployee full = new FullTimeEmployee();
+            full.EmployeeId = 1002;
+            full.FirstName = "Buster";
+            full.LastName = "Douglass";
+            full.PrintFullName();
+
+            PartTimeEmployee pa = new PartTimeEmployee();
+            pa.EmployeeId = 5209;
+            pa.FirstName = "Mike";
+            pa.LastName = "Tyson";
+            pa.PrintFullName();
+
+
+            Console.ReadLine();
+        }
+    }
+}
