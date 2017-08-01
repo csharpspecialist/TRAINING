70d884441271c21e94a967ecf36d47958c70d51c
csharpspecialist <netspec1@hotmail.com>
2017-08-01 19:18:10 -0400

    cleaned spaces in the AbstractApp1 project

--- AbstractApp1/AbstractApp1/Program.cs
+++ AbstractApp1/AbstractApp1/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace AbstractApp1
 {
-    class Program
+    
+    public class Program 
     {
         static void Main(string[] args)
         {
         }
     }
+
+    public abstract class Customer 
+    {
+        //can only be used as base classes.
+        //cannot have implementation here!!!! only declaration!!
+        public abstract void Ordering();
+
+
+    }
+
+
+
+
+        //since the testing class is marked abstract then 
+        // it does not have to provide implementation for 
+        // the Customer class. this class must be public!!!
+    public abstract class Testing : Customer
+    {
+
+    }
+
+    public class Worker : Customer
+    {
+        //this must override the ordering method in customer class
+        //to provide to compile and work properly!!! 
+        public override void Ordering() 
+        {
+            Console.WriteLine("We r working over here");
+        }
+
+        
+
+    }
+    
+
+
 }
