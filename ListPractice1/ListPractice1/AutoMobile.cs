b918d4fdb2b3e9e3418572e04e4889a676526573
CCC <RAIDEN@DEBO>
2017-08-08 08:58:05 -0400

    c up spacesin List practice 1 proj

---
+++ ListPractice1/ListPractice1/AutoMobile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPractice1
+{
+    class AutoMobile
+    {
+
+        public int AutoID { get; set; }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        //AutoImplemented property
+      
+
+        private string trimLevel;
+
+        public string TrimLevel
+        {
+            get { return trimLevel; }
+            set { trimLevel = value; }
+        }
+
+
+
+    }
+}
---
+++ ListPractice1/ListPractice1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoMobile a1 = new AutoMobile();
+
+            a1.AutoID = 1000;
+            a1.Make = "Chevy";
+            a1.Model = "Corvette";
+            a1.Year = 2009;
+            a1.Color = "Red";
+
+            AutoMobile a2 = new AutoMobile();
+
+            a2.AutoID = 2000;
+            a2.Make = "Ford";
+            a2.Model = "Explorer";
+            a2.Year = 2012;
+            a2.Color = "Black";
+
+            List<AutoMobile> carList = new List<AutoMobile>();
+
+            carList.Add(a1);
+            carList.Add(a2);
+
+            foreach (AutoMobile item in carList)
+            {
+                Console.WriteLine($"Our Id is {item.AutoID}, made by {item.Make}, in the year {item.Year}, Model type of {item.Model} with the color is {item.Color}");
+            }
+
+            Console.ReadLine();
+
+            foreach (AutoMobile item in carList)
+            {
+               if(item.Make == "Ford")
+                {
+                    Console.WriteLine($"We found the {item.Model} ");
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+  
+
+
+}
