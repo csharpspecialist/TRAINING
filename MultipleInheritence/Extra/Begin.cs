4e64ceba763ea105da7a4fb7554496156538d21a
CCC <RAIDEN@DEBO>
2017-08-11 10:55:32 -0400

    com Multiple Inthertence class proj

--- MultipleInheritence/Extra/Begin.cs
+++ MultipleInheritence/Extra/Begin.cs
@@ -3,18 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MultipleInheritence;
+
 
 namespace Extra
 {
-  public  class Begin
+  public class Begin
     {
         static void Main(string[] args)
         {
+            int complete;
+            Console.WriteLine("Hello...its Friday!!!");
+            Durango dr = new Durango();
 
+            complete = dr.PerformAddition(80,100);
 
+            Console.WriteLine(complete);
+            Console.ReadLine();
+        }
+        
 
+    }
+
+    public class Durango
+    {
+       
+        public void Boxer()
+        {
+            Console.WriteLine("We Work to be the best programmers in the world");
 
         }
+
+        public int PerformAddition(int x, int y)
+        {
+            
+            return x + y;
+        }
+
+      
+
     }
+
+
 }
--- MultipleInheritence/MultipleInheritence/Program.cs
+++ MultipleInheritence/MultipleInheritence/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Extra;
+
 
 namespace MultipleInheritence
 {
@@ -17,6 +19,33 @@ namespace MultipleInheritence
 
             Console.ReadLine();
 
+            Durango d = new Durango();
+            int total;
+            int sum;
+
+         total =   d.PerformAddition(5,10);
+
+            Console.WriteLine(total);
+            Special();
+         
+
+            Console.ReadLine();
         }
+
+
+        public static void Special()
+        {
+            int sum;
+            int total2;
+            Durango dur = new Durango();
+            dur.Boxer();
+
+          total2 =  dur.PerformAddition(20,200);
+
+            Console.WriteLine(total2);
+
+        }
+
+
     }
 }
