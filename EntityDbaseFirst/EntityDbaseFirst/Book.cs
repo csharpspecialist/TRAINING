8ddd54f45e521b3771a58ffb331aa3069cba8921
csharpspecialist <netspec1@hotmail.com>
2017-09-14 23:18:01 -0400

    9-14-2017 Class Demo

---
+++ EntityDbaseFirst/EntityDbaseFirst/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //must add in this classes
+using System.Data.Entity; //must add in this  class!!!
+
+namespace EntityDbaseFirst
+{
+    public class Book // must be public or squiggly @ dbset book books!!!
+    {
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Key]
+        public int Code { get; set; }
+
+
+    }
+
+
+    public class BookContext : DbContext
+    {
+
+
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
---
+++ EntityDbaseFirst/EntityDbaseFirst/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+
+namespace EntityDbaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BookContext()) 
+
+            { Book book1 = new Book { Title = "Beginning C # 2015", Author = "Perkins" };
+
+                db.Books.Add(book1);
+                Book book2 = new Book { Title = "Beginning Xml", Author = "Fawcett" };
+
+                db.Books.Add(book2);
+                db.SaveChanges();
+
+                var query = from b in db.Books
+                            orderby b.Title
+                            select b;
+                WriteLine("All books in the database:");
+                foreach (var b in query)
+                {
+                    WriteLine($"{b.Title} by {b.Author}, code={ b.Code}");
+                }
+                WriteLine("Press a key to exit…");
+                ReadKey();
+
+
+            }
+        }
+    }
+}
