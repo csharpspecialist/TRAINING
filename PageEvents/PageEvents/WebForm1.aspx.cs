b2a62566f3acabb3c66e5082c3c91d5c17528cd5
CCC <RAIDEN@DEBO>
2017-09-15 09:05:10 -0400

    CC4 Wiley PageEvents Book Demo

---
+++ PageEvents/PageEvents/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace PageEvents
+{
+    public partial class WebForm1
+    : System.Web.UI.Page
+    {
+        protected void Page_Load
+        (object sender, EventArgs e)
+        {
+            Response.Write
+            ("Message from Page_Load. <br/>");
+        }
+        protected void Page_Init
+        (object sender, EventArgs e)
+        {
+            Response.Write
+            ("Message from Page_Init. <br/>");
+        }
+        protected void Page_PreRender
+        (object sender, EventArgs e)
+        {
+            Response.Write
+            ("Message from Page_PreRender. <br/>");
+        }
+        protected void Page_PreInit
+        (object sender, EventArgs e)
+        {
+            Response.Write
+            ("Message from Page_PreInit. <br/>");
+        }
+    }
+}
