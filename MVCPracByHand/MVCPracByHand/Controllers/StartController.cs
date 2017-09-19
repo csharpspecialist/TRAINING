309b26e0ecd21ce051aba55f973a4240b8dc3af0
csharpspecialist <netspec1@hotmail.com>
2017-09-19 01:42:19 -0400

    building an MVC Project from scratch

---
+++ MVCPracByHand/MVCPracByHand/Controllers/StartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPracByHand.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Start
+        public ActionResult What()
+        {
+            return View();
+        }
+
+    }
+}
