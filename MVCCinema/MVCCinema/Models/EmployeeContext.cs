0fad27d7afa3ec72febc1b7bbdd26badf98ffe18
csharpspecialist <netspec1@hotmail.com>
2017-09-21 18:18:20 -0400

    trying to conn 2 dbase that already exists

---
+++ MVCCinema/MVCCinema/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCCinema.Models
+{
+    public class EmployeeContext : DbContext
+    {
+
+        public DbSet<Region> Regions { get; set; }
+
+    }
+}
