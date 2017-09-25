8426a5d59ab4d25c7cd13161b16124dad6737f2d
CCC <RAIDEN@DEBO>
2017-09-25 10:00:45 -0400

    Add a column to an exist MVC app MVC Add Column

---
+++ MVCPractice/MVCPractice/Controllers/CustomersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCPractice.Models;
+
+namespace MVCPractice.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private CustomerDBContext db = new CustomerDBContext();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.Cust.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Cust.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,LastName,FirstName,Address,Age")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cust.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customers);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Cust.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,Address,Age")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customers);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Cust.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customers customers = db.Cust.Find(id);
+            db.Cust.Remove(customers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCPractice/MVCPractice/Models/Customers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCPractice.Models
+{
+    public class Customers
+    {
+
+        public int ID { get; set; }
+        [ Display(Name = "Last Name") ]
+        public string LastName { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+    }
+
+    public class CustomerDBContext: DbContext
+    {
+
+        public DbSet<Customers> Cust { get; set; }
+    }
+
+}
