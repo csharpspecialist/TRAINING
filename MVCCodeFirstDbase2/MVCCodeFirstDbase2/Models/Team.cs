ac25c4825b5da7ec05fd73d507cf640cd842ddcd
csharpspecialist <netspec1@hotmail.com>
2017-09-30 02:35:17 -0400

    tried to use Udl file...No happening. Stick w xman conn string!!!!

---
+++ MVCCodeFirstDbase2/MVCCodeFirstDbase2/Models/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCCodeFirstDbase2.Models
+{
+    public class Team
+    { 
+        public int TeamID { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string City { get; set; }
+        public int NetWorth { get; set; }
+       
+        
+
+    }
+
+    public class TeamDBContext : DbContext
+    {
+        public DbSet<Team> Tough { get; set; }
+    }
+
+
+}
