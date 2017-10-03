adcd8cd9ef243b827adf534abbc6df26b5665b67
CCC <RAIDEN@DEBO>
2017-10-03 11:46:42 -0400

    Cr 2 webform apps for class demo

---
+++ WebformDbaseConnProj1/WebformDbaseConnProj1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebformDbaseConnProj1.Startup))]
+namespace WebformDbaseConnProj1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
