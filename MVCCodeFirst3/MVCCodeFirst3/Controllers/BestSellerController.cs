9142a382a8dc575588470b24dc244f6e124372b8
csharpspecialist <netspec1@hotmail.com>
2018-03-07 00:50:46 -0500

    created new MVC Code First 3 Proj

---
+++ MVCCodeFirst3/MVCCodeFirst3/Controllers/BestSellerController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCCodeFirst3.Models;
+
+namespace MVCCodeFirst3.Controllers
+{
+    public class BestSellerController : Controller
+    {
+        private BooksDBContext db = new BooksDBContext();
+
+        // GET: BestSeller
+        public ActionResult Index(string searchString)
+        {
+            //string searchString = id;
+            var bibles = from b in db.Novels
+                         select b;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                bibles = bibles.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(db.Novels.ToList());
+        }
+
+
+        [HttpPost]
+        public string Index(FormCollection fc, string searchString)
+        {
+            return "<h3> From [HttpPost]Index: " + searchString + "</h3>";
+        }
+
+
+
+
+        // GET: BestSeller/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // GET: BestSeller/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BestSeller/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Author,Pages,Genre,Price")] Novel novel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Novels.Add(novel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(novel);
+        }
+
+        // GET: BestSeller/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // POST: BestSeller/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Author,Pages,Genre,Price")] Novel novel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(novel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(novel);
+        }
+
+        // GET: BestSeller/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // POST: BestSeller/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Novel novel = db.Novels.Find(id);
+            db.Novels.Remove(novel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCCodeFirst3/MVCCodeFirst3/Models/Novel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCCodeFirst3.Models
+{
+    public class Novel
+    {      
+     
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }            
+        public string Genre { get; set; }
+        public decimal Price { get; set; }   
+        
+    }
+
+    public class BooksDBContext : DbContext
+    {
+        public DbSet<Novel> Novels { get; set; }
+    }
+
+
+
+}
